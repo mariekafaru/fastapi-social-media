6f9170db7d8b96e68a36be86e686f1a6be93d20e
mariekafaru <mariekafaru@gmail.com>
2025-06-02 18:04:50 +0100

    Fix uvicorn port config for Render deployment

--- src/app.py
+++ src/app.py
@@ -7,14 +7,33 @@ from .social_media.auth import router as login_router
 from .social_media.users import user_router
 from .social_media.vote import router as vote_router
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import os
 
 
 app = FastAPI()
 
-models.Base.metadata.create_all(engine)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],
+)
+
+#models.Base.metadata.create_all(engine)
 
 app.include_router(post_router)
 app.include_router(user_router)
 app.include_router(login_router)
 app.include_router(vote_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",  
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000))
+    )
+
 
